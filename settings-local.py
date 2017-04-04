3587ab640cf7f473f073fe394f7ad59c022c8166
quovadimus <quovadimus@users.noreply.github.com>
2017-04-04 08:57:34 -0400

    Create settings-local.py

---
+++ settings-local.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+PROJECT_NAME = 'my_test_dashboard'
+API_KEY = '0eb162857ab54468beb5d5d37906a85e'
+
+
+FLIPBOARD_INTERVAL = 10
+FLIPBOARD_SEQUENCE = ['layout_config', 'layout_two', 'layout_test']
